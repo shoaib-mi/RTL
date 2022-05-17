714848c0b5e4b8bae9e1555b4cc1e86f7c949e86
shoaib-mi <62565867+shoaib-mi@users.noreply.github.com>
2022-05-17 20:32:02 +0430

    Update test.nim

--- test.nim
+++ test.nim
@@ -1,12 +1,18 @@
-import nimPDF/nimPDF, RTL
+import nimPDF/nimPDF, RTL, os
+
 var opts = newPDFOptions()
-opts.addFontsPath(r"C:\Windows\Fonts")
+when defined windows:
+    opts.addFontsPath(r"C:\Windows\Fonts")
+when defined linux:
+    var home = getHomeDir()    
+    opts.addFontsPath(home & "/.local/share/fonts")
+
 var doc = newPDF(opts)
 doc.addPage(getSizeFromName("A4"), PGO_PORTRAIT)
 doc.setFont("B Nazanin", {FS_REGULAR}, 5, ENC_UTF8)
 
 var line: string
-line = "متن آزمایشی"
-doc.drawText(15, 50, str2uni(line))
+line = str2uni("متن آزمایشی")
+doc.drawText(15, 50, line)
 if not doc.writePDF(r"hello.pdf"):
     echo "cannot open: hello.pdf"
